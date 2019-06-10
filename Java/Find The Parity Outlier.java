dc822d174fac4d70801424ae617d5d2ffcaf1961
Yowen <39782590+Yowen@users.noreply.github.com>
2019-06-10 11:04:47 -0400

    Update Find The Parity Outlier.java

--- Java/Find The Parity Outlier.java
+++ Java/Find The Parity Outlier.java
@@ -1,24 +1,24 @@
 import java.util.*;
 
 public class FindOutlier{
-  static int find(int[] integers) {
-    List<Integer> even = new ArrayList<Integer>();
-    List<Integer> odd = new ArrayList<Integer>();
+    static int find(int[] integers) {
+        List<Integer> even = new ArrayList<Integer>();
+        List<Integer> odd = new ArrayList<Integer>();
     
-    for (int i = 0; i < integers.length; i++) {
-      if (integers[i] % 2 == 0) {
-        even.add(integers[i]);
-      }
-      else {
-        odd.add(integers[i]);
-      }
-    }
+        for (int i = 0; i < integers.length; i++) {
+            if (integers[i] % 2 == 0) {
+                even.add(integers[i]);
+            }
+             else {
+                odd.add(integers[i]);
+            }
+        }
     
-    if (even.size() > odd.size()) {
-      return odd.get(0);
+        if (even.size() > odd.size()) {
+            return odd.get(0);
+        }
+        else {
+            return even.get(0);
+        }
     }
-    else {
-      return even.get(0);
-    }
-  }
 }
